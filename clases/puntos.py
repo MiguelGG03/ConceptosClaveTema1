4beb8d111adb13196a73cda1b32f549b3dc1b327
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-21 17:18:30 +0200

    tanteado

--- clases/puntos.py
+++ clases/puntos.py
@@ -1,11 +1,17 @@
 class Punto:
 
+    p_existentes=[]
+
     def __init__(self,x,y):
-        if(x==None):
-            self.x=0
-        else:
-            self.x=x
-        if(y==None):
-            self.y=0
-        else:
-            self.y=y
+        slef
+
+    def __del__ (self):
+        i= Punto.p_existentes.index(self.x)
+        del (Punto.p_existentes[i])
+
+    def setIDx(self,x):
+
+
+    def setIDy(self,y):
+
+    def set_x(self):
