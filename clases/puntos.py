b1b6b67141344357a4f793ce8adf59368928d298
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-21 17:19:30 +0200

    Setters

--- clases/puntos.py
+++ clases/puntos.py
@@ -3,15 +3,17 @@ class Punto:
     p_existentes=[]
 
     def __init__(self,x,y):
-        slef
+        self.x=xself.y=y
 
     def __del__ (self):
         i= Punto.p_existentes.index(self.x)
         del (Punto.p_existentes[i])
 
     def setIDx(self,x):
-
+         self.x = int(input("Introduce la comp X"))
 
     def setIDy(self,y):
+        self.y =  int(input("Introduce la comp Y "))
 
-    def set_x(self):
+    def __str__(self):
+        return ( str(self.x)   + str(self.y) )
