3189fcae57fc5cd464b81b6590f81f8f5dce390f
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-21 18:28:41 +0200

    finalizado creo

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from clases.puntos import Punto as p
 def main():
     seguir=True
     while(seguir==True):
-        a=input('Coordenadas X:')
-        b=input('Coordenadas Y:')
+        a=input('Coordenadas p1 X:')
+        b=input('Coordenadas p1 Y:')
+        c=input('Coordenadas p2 Y:')
+        d=input('Coordenadas p2 Y:')
         point = p(a,b)
+        vector=point.vector(c,d)
+        point.setIDvx()
+        point.setIDvy()
+        vector
         point.setIDx()
         point.setIDy()
         print('------------CARACTERISTICAS DEL PUNTO------')
         point.string()
+        print('------------CARACTERISTICAS DEL VECTOR------')
+        point.distancia()
         print('------------CUADRANTE DEL PUNTO------------')
         point.cuadrante()
         preg=input('Desea seguir poniendo puntos? 0=NO 1=SI :')
