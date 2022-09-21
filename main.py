2ff9716d15abff86edf2b62310e14e0288b6d0ee
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-21 18:03:38 +0200

    main cambiado

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from clases.puntos import Punto as p
 
 def main():
-    a=input('Coordenadas X:')
-    b=input('Coordenadas Y:')
-    point = p(a,b)
-    point.setIDx()
-    point.setIDy()
+    seguir=True
+    while(seguir==True):
+        a=input('Coordenadas X:')
+        b=input('Coordenadas Y:')
+        point = p(a,b)
+        point.setIDx()
+        point.setIDy()
+        point.añadir_a_la_lista()
+        preg=input('Desea seguir poniendo puntos? 0=NO 1=SI :')
+        if(preg==0):
+            seguir=False
+        elif(preg==1):
+            seguir=True
+        else:
+            print('Eso no es un 1 o un 2, asimilamos un 1 como respuesta')    
+    
+    
+    p.chequear_p_existentes()
     point.__str__()
 
 
